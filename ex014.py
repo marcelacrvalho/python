353a9384fb9eb6eb159a76e083498076dcb62b00
Marcela <marcelacrv@icloud.com>
2019-01-21 18:49:56 -0200

    Create ex014.py

---
+++ ex014.py
@@ -0,0 +1,8 @@
+a = float(input("Tamanho da reta A: "))
+b = float(input("Tamanho da reta B: "))
+c = float(input("Tamanho da reta C: "))
+
+if a == c and b == 2 * a:
+    print("Com estas retas é possível formar um triângulo :)")
+else:
+    print("Com estas retas não é possível formar um triângulo")
