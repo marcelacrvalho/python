d5fc47c14e0afe9f4897fd4bb98e1cef408103a3
Marcela <marcelacrv@icloud.com>
2019-01-23 14:58:02 -0200

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -3,5 +3,5 @@ sum = 0
 for i in range(0, 6):
     num = int(input("Enter a number: "))
     if num % 2 != 0:
-        soma += num
+        sum += num
 print("The sum of the odd numbers entered was {}".format(sum))
